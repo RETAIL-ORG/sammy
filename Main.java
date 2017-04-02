f6dfc20bb080ed8481068433e7ec33727a124ad7
titan1978 <titan1978@users.noreply.github.com>
2017-04-02 14:00:42 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 public class Main {
 	public static void main(String argv[]) {
 		System.out.println("Fixed a bug in bau"); // Added a comment in emr
-		// Added a comment in sprint
-		// Added a new comment
 	}
 }
